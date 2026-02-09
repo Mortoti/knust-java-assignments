0acf4cef8facceaf317ce9a5b67ef3f2f3343481
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2026-02-09 23:12:03 +0000

    Question1

--- src/Question1.java
+++ src/Question1.java
@@ -1,28 +1,35 @@
 public class Question1 {
     public static void main(String [] args){
-    double [] prices = {15.0, 55.0, 120.0, 200.0};
-    double totalBill = 0.0;
-    boolean isDiscounted = false;
-    double discount =  0.0;
+        double [] prices = {15.0, 55.0, 120.0, 200.0};
+        double totalBill = 0.0;
 
-    for (int i = 0; i <=3; i ++){
-        double currentPrice = 0.0;
-        currentPrice = prices [i];
+        // 0, 1, 2, 3, 4
 
-        if (currentPrice >=100){
-            discount = 0.2 * currentPrice;
-            currentPrice =currentPrice - discount ;
-            isDiscounted = true;
+        for (int i = 0; i <= (prices.length -1); i ++) {
+            boolean isDiscounted = false;
+            double currentPrice = prices[i];
+
+            if (currentPrice > 100) {
+                double discount = 0.2 * currentPrice; // 24
+                currentPrice = currentPrice - discount;
+                isDiscounted = true;
+                totalBill += currentPrice;
+
+            }
+            else{
+                totalBill += currentPrice;
+            }
+            System.out.println((i + 1) + ". " + currentPrice);
+            if (isDiscounted){
+                System.out.println("20% Discount Applied!");
+
+            }
+
+
         }
-        totalBill += currentPrice;
-        System.out.println(i + 1 + ". " + currentPrice);
-        if (isDiscounted){
-            System.out.println("20 % Discount Applied!");
-        }
-
-    }
-
         System.out.println("The total bill is: " + totalBill);
+
+
 
 
     }
