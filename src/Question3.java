c1f6a9bd0e6c81b4381a9e9c76d43e46a56f3868
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2026-02-09 23:13:01 +0000

    Question3

--- src/Question3.java
+++ src/Question3.java
@@ -2,28 +2,30 @@ import java.util.Scanner;
 public class Question3 {
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in);
-        double oxygenTank = 500.00;
+        double oxygenTank = 500.0;
         int diverCount = 0;
-        while (oxygenTank > 50.00){
-            System.out.print("Enter the oxygen needed for this dive in Liters: ");
+        while(oxygenTank > 50){
+            System.out.print("Enter the amount of oxygen needed for the dive: ");
             double oxygenNeeded = scanner.nextDouble();
             if (oxygenNeeded > oxygenTank){
                 System.out.println("Insufficient oxygen for this dive!");
-                break;
+                break; // continue
             }
-            else{
+            else {
                 oxygenTank -= oxygenNeeded;
                 diverCount += 1;
-                System.out.println("Dive Successful");
+                System.out.println("Dive Successful!");
+
             }
             if (oxygenTank < 150){
-                System.out.println("WARNING: Oxygen level at " + oxygenTank);
-                System.out.println("Return to surface soon");
+                System.out.println("WARNING: Oxygen level at " + oxygenTank + " - Return to surface soon.");
             }
+
         }
-        System.out.println("Total Successful Dives: " + diverCount);
-        System.out.println("Remaining Oxygen: " + oxygenTank + " Liters");
+        System.out.println("Total Dives: " + diverCount);
+        System.out.println("Remaining Oxygen: " + oxygenTank);
         scanner.close();
+
 
     }
 
